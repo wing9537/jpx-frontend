0311521a36fee0039632091769928851bb15eb7c
williamphk <65807958+williamphk@users.noreply.github.com>
2023-01-15 11:56:51 -0500

    Build login dialog ui

--- src/components/dialog/LoginDialog.js
+++ src/components/dialog/LoginDialog.js
@@ -1,15 +1,11 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import Dialog from "@mui/material/Dialog";
-import PersonIcon from "@mui/icons-material/Person";
-import AddIcon from "@mui/icons-material/Add";
-import { blue } from "@mui/material/colors";
+import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import { Link } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 
 // self component
@@ -30,39 +26,34 @@ function LoginDialog() {
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {emails.map((email) => (
-          <ListItem disableGutters>
-            <ListItemButton
-              onClick={() => handleListItemClick(email)}
-              key={email}
-            >
-              <ListItemAvatar>
-                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                  <PersonIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={email} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+    <Dialog onClose={handleClose} open={open} maxWidth='xs'>
+      <DialogTitle sx={{ fontSize: 25, fontWeight: 'bold', mb: -1 }}>Welcome!</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Sign in to continue.</DialogContentText>
+        <TextField
+          autoFocus
+          margin="normal"
+          id="email"
+          label="Email Address"
+          type="email"
+          placeholder="example@email.com"
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          autoFocus
+          margin="normal"
+          id="password"
+          label="Password"
+          type="password"
+          placeholder="your password"
+          fullWidth
+          variant="outlined"
+        />
+        <Button variant="contained" fullWidth sx={{ my: 2 }}>Log in</Button>
+        <DialogContentText>Don't have a account? <Link underline="hover" href="/sign-up"> Sign up</Link></DialogContentText>
 
-        <ListItem disableGutters>
-          <ListItemButton
-            autoFocus
-            onClick={() => handleListItemClick("addAccount")}
-          >
-            <ListItemAvatar>
-              <Avatar>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Add account" />
-          </ListItemButton>
-        </ListItem>
-      </List>
+      </DialogContent>
     </Dialog>
   );
 }
