9fca6bb3026aa4c9af809f1ab425925da5f0c772
wing9537 <tommywing37@gmail.com>
2023-02-02 03:20:12 +0000

    remove pattern

--- src/components/inputs/baseInput.js
+++ src/components/inputs/baseInput.js
@@ -65,7 +65,6 @@ function BaseInput({
             maxLength,
             minLength,
             validate: rules,
-            pattern,
           })}
           id={`txt-${name}`}
           label={mandatoryLabel}
@@ -83,7 +82,6 @@ function BaseInput({
             maxLength,
             minLength,
             validate: rules,
-            pattern,
           })}
           id={`pwd-${name}`}
           label={mandatoryLabel}
